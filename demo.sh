1614873e1bb6a0e1143505e89d9e5672872bffa8
nvidintelamd <910492911@qq.com>
2018-08-06 09:41:35 +0800

    bash脚本作业 - 添加判断文件是否存在的结构

--- demo.sh
+++ demo.sh
@@ -1,10 +1,14 @@
-mkdir $1
-cd $1
-mkdir css js
-echo "<!DOCTYPE>
-<title>Hello</title>
-<h1>Hi</h1>" >> index.html
-echo "h1{color: red;}" >> css/style.css
-echo "var string = \"Hello World\"
-alert(string)" > js/main.js 
-exit
+if [ -d $1 ]; then
+  exit 1
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  echo "<!DOCTYPE>
+  <title>Hello</title>
+  <h1>Hi</h1>" >> index.html
+  echo "h1{color: red;}" >> css/style.css
+  echo "var string = \"Hello World\"
+  alert(string)" > js/main.js 
+  exit 0
+fi
